b12b414a6607d25443ea2ffbc41865c8a55fea0f
Mikah Mathews <mikah@outofband.org>
2022-04-22 11:22:52 -0700

    Add completed keg.js

--- src/components/kegs/Keg.js
+++ src/components/kegs/Keg.js
@@ -4,3 +4,26 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
+function Keg(props) {
+  return (
+    <React.Fragment>
+      <ListGroup.Item className="mb-3 border shadow-sm">
+        <Row>
+          <Col className="pt-2 pb-2 position-relative">
+            <h4 className="fw-normal border-bottom pb-1">{props.name}<span className="fw-light">|</span> <span className="fs-6">({props.pints} Pints Available)</span></h4>
+            Brand: {props.brand}
+            Bean Type: {props.beanType}
+            {props.description}
+
+            <div className="mt-3 me-3 text-end position-absolute bottom-0 end-0">
+              <Button variant="dark" size="sm" className="me-3 px-4 rounded-pill shadow-sm">Add to Cart</Button>
+              <Button variant="dark" size="sm" className="px-4 rounded-pill shadow-sm">Edit</Button>
+            </div>
+          </Col>
+        </Row>
+      </ListGroup.Item> 
+    </React.Fragment>
+  );
+}
+
+export default Keg;
