337aea0e89824df3578c355cc343295ed54e1686
Mikah Mathews <mikah@outofband.org>
2022-04-22 10:37:35 -0700

    Add base file structure

---
+++ src/components/kegs/Keg.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
---
+++ src/components/kegs/KegForm.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import PropTypes from "prop-types";
+import { v4 } from 'uuid';
