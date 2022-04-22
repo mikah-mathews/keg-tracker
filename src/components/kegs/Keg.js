8d729516a751beaf095dedb0bd47201c40e5936f
Mikah Mathews <mikah@outofband.org>
2022-04-22 15:39:38 -0700

    Revert KegContainer to original style and delete List/Details

--- src/components/kegs/Keg.js
+++ src/components/kegs/Keg.js
@@ -3,7 +3,6 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import PropTypes from 'prop-types';
 
 function Keg(props) {
   return (
@@ -11,9 +10,15 @@ function Keg(props) {
       <ListGroup.Item className="mb-3 border shadow-sm">
         <Row>
           <Col className="pt-2 pb-2 position-relative">
-            <div onClick = {() => props.whenKegClicked(props.id)}>
-              <h4 className="fw-normal border-bottom pb-1">{props.name}<span className="fw-light">|</span> <span className="fs-6">({props.pints} Pints Available)</span></h4>
-              Brand: {props.brand}<br />
+            <h4 className="fw-normal border-bottom pb-1">{props.name}<span className="fw-light">|</span> <span className="fs-6">({props.pints} Pints Available)</span></h4>
+            Brand: {props.brand}<br />
+            Bean Type: {props.beanType} <br />
+            Pint Price: $ {props.price} <br />
+            {props.description}<br /><br /><br />
+
+            <div className="mt-3 me-3 text-end position-absolute bottom-0 end-0">
+              <Button variant="dark" size="sm" className="me-3 px-4 rounded-pill shadow-sm">Buy a Pint</Button>
+              <Button variant="dark" size="sm" className="px-4 rounded-pill shadow-sm">Edit</Button>
             </div>
           </Col>
         </Row>
@@ -22,7 +27,4 @@ function Keg(props) {
   );
 }
 
-Keg.propTypes = {
-  whenTicketClicked: PropTypes.func
-}
 export default Keg;
--- src/components/kegs/KegParentContainer.js
+++ src/components/kegs/KegParentContainer.js
@@ -3,31 +3,16 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
-import KegList from './KegList';
+import Keg from './Keg';
 import KegForm from './KegForm';
-import KegDetails from './KegDetails';
 
 class KegParentContainer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       mainKegList: [],
-      formVisibleOnPage: false,
       selectedKeg: null
     };
-  }  
-
-  handleClick = () => {
-    if (this.state.selectedKeg != null) {
-      this.setState({
-        formVisibleOnPage: false,
-        selectedKeg: null
-      });
-    } else {
-      this.setState(prevState => ({
-        formVisibleOnPage: !prevState.formVisibleOnPage
-      }));
-    }
   }
 
   handleAddingNewKegToList = (newKeg) => {
@@ -37,31 +22,33 @@ class KegParentContainer extends React.Component {
     });
   }
 
+
   render() {
-    let currentlyVisibleState = null;
-    let buttonText = null;
-
-    if (this.state.selectedKeg != null) {
-      currentlyVisibleState = <KegDetails keg = {this.state.selectedKeg} />
-      buttonText = "Return to Keg List"
-    } else if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <Col md={4} className="py-3 px-4 fw-light">
-        <Row className="text-center border rounded shadow-sm pb-5 pt-3 px-3">
-          <Col>
-            <KegForm onNewKegCreation={this.handleAddingNewKegToList} />
-          </Col>
-        </Row>
-      </Col>
-      buttonText = "Return to Keg List"
-    } else {
-      currentlyVisibleState = <KegList kegList={this.state.mainKegList} onKegSelection={this.handleChangingSelectedKeg} />
-      buttonText="Add New Keg"
-    }
-
     return (
       <Container className="pt-2">
-        {currentlyVisibleState}
-        <Button variant="dark" size="lg" type="submit" className="px-4 rounded-pill shadow-sm">{buttonText}</Button>
+        <Row className="border rounded shadow-sm">
+          <Col md={8} className="p-3 fw-light">
+            <ListGroup>
+              {this.state.mainKegList.map((keg) =>
+                <Keg name={keg.name}
+                brand={keg.brand}
+                description={keg.description}
+                price={keg.price}
+                beanType={keg.beanType}
+                pints={keg.pints} 
+                key={keg.id}
+                />
+              )}
+            </ListGroup>
+          </Col>
+          <Col md={4} className="py-3 px-4 fw-light">
+            <Row className="text-center border rounded shadow-sm pb-5 pt-3 px-3">
+              <Col>
+                <KegForm onNewKegCreation={this.handleAddingNewKegToList} />
+              </Col>
+            </Row>
+          </Col>
+        </Row>
       </Container>
     );
   }
